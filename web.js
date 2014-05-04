7c29dd124f1524fd1bf7c92311f2a2235bfb2f8f
bomatson <bobby@carbonfive.com>
2014-05-04 10:51:10 -0700

    configure the manatees to swim with jade

--- web.js
+++ web.js
@@ -1,11 +1,17 @@
 var express = require("express");
 var logfmt = require("logfmt");
+//var leapPlugins = require('leapjs-plugins');
+//var controller = new Leap.Controller()
+
 var app = express();
 
 app.use(logfmt.requestLogger());
 
+app.set('views', __dirname + '/views')
+app.set('view engine', 'jade')
+
 app.get('/', function(req, res) {
-    res.render(root);
+  res.render('root', { title: 'Manatees!' });
 });
 
 var port = Number(process.env.PORT || 5000);

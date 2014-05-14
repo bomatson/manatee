64d03d413fb07fde083e179a4ae3bb2622c1c659
bomatson <bobby@carbonfive.com>
2014-05-14 15:51:02 -0700

    use the spritesheet and get funky with the lettuce

---
+++ public/js/game.js
@@ -0,0 +1,95 @@
+var WIDTH = window.innerWidth;
+var HEIGHT = window.innerHeight;
+var manatee, delta;
+var stage = new PIXI.Stage(0xEEFFFF);
+var lettuce = [];
+
+
+var renderer = PIXI.autoDetectRenderer(WIDTH, HEIGHT);
+
+document.body.appendChild(renderer.view);
+
+var loader = new PIXI.AssetLoader(['/images/spritesheet.json'], true);
+
+loader.onComplete = loadGameArtifacts;
+loader.load();
+
+function loadGameArtifacts() {
+  renderManatee();
+  stage.addChild(new Lettuce().render());
+  requestAnimFrame(gameLoop);
+}
+
+var Lettuce = function() {
+  this.render =  function() {
+    var image = PIXI.Sprite.fromFrame('ood.png');
+    image.anchor.x = 0.5;
+    image.anchor.y = 0.5;
+    image.position.x = 100;
+    image.position.y = 100;
+    stage.addChild(image);
+    image.updateMovement = this.updateMovement;
+    image.checkBounds = this.checkBounds;
+    return image
+  },
+
+  this.down = true,
+
+  this.updateMovement = function() {
+    this.position.x += 0.2 * delta;
+    var randomNumber = 0.2;
+    if(this.down){
+      if( this.position.y <= (HEIGHT - 100)){
+        this.position.y += randomNumber * delta;
+      }else{
+        this.down  = false;
+      }
+    }else{
+      if( this.position.y >= 10){
+        this.position.y -= randomNumber * delta;
+      }else{
+        this.down  = true;
+      }
+    }
+  },
+
+  this.checkBounds = function() {
+    if(this.position.x > WIDTH) {
+      stage.removeChild(this);
+    }
+  }
+}
+
+function renderManatee() {
+  manatee = PIXI.Sprite.fromFrame('anatee-main.png');
+
+  manatee.anchor.x = 0.5;
+  manatee.anchor.y = 0.5;
+
+  manatee.position.x = WIDTH / 2;
+  manatee.position.y = HEIGHT / 2;
+
+  stage.addChild(manatee);
+}
+
+var timer = window.performance.now();
+
+function gameLoop() {
+  var now = window.performance.now();
+  delta = Math.min(now - timer, 20);
+  timer = now;
+  for(i=0; i< stage.children.length; i++) {
+    if(stage.children[i].updateMovement !== undefined) {
+      stage.children[i].updateMovement();
+      stage.children[i].checkBounds();
+    }
+  };
+  if(stage.children.length < 10) {
+    new Lettuce().render();
+  };
+  var point = stage.getMousePosition();
+  manatee.position.x = point.x;
+  manatee.position.y = point.y;
+  requestAnimFrame(gameLoop);
+  renderer.render(stage);
+}

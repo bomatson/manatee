71a836ca2c330f89ce9515ab3bff27d68e5fdb83
bomatson <bobby@carbonfive.com>
2014-05-14 19:01:39 -0700

    more lettuce

--- public/js/game.js
+++ public/js/game.js
@@ -23,40 +23,44 @@ function loadGameArtifacts() {
 var Lettuce = function() {
   this.render =  function() {
     var image = PIXI.Sprite.fromFrame('ood.png');
+    var randomY = (Math.floor((Math.random() * 300) + 1));
+
     image.anchor.x = 0.5;
     image.anchor.y = 0.5;
-    image.position.x = 100;
-    image.position.y = 100;
+    image.position.x = 10;
+    image.position.y = randomY;
     stage.addChild(image);
+
     image.updateMovement = this.updateMovement;
     image.checkBounds = this.checkBounds;
+    speed = (Math.random() * 0.3) + 0.1;
     return image
-  },
+  };
 
-  this.down = true,
-
+  var down = true;
+  var speed = null;
   this.updateMovement = function() {
-    this.position.x += 0.2 * delta;
+    this.position.x += 0.2 * delta * speed;
     var randomNumber = 0.2;
-    if(this.down){
+    if(down){
       if( this.position.y <= (HEIGHT - 100)){
         this.position.y += randomNumber * delta;
       }else{
-        this.down  = false;
+        down  = false;
       }
     }else{
       if( this.position.y >= 10){
         this.position.y -= randomNumber * delta;
       }else{
-        this.down  = true;
+        down  = true;
       }
     }
-  },
+  };
 
   this.checkBounds = function() {
-    if(this.position.x > WIDTH) {
+    if(this.x > (WIDTH - 10) || this.getBounds().contains(manatee.x, manatee.y)) {
       stage.removeChild(this);
-    }
+    };
   }
 }
 
@@ -68,6 +72,8 @@ function renderManatee() {
 
   manatee.position.x = WIDTH / 2;
   manatee.position.y = HEIGHT / 2;
+
+  manatee.hitArea = new PIXI.Rectangle(30, 30, 100 ,100);
 
   stage.addChild(manatee);
 }
@@ -84,10 +90,10 @@ function gameLoop() {
       stage.children[i].checkBounds();
     }
   };
-  if(stage.children.length < 10) {
+  if(stage.children.length < 1500) {
     new Lettuce().render();
   };
-  var point = stage.getMousePosition();
+ var point = stage.getMousePosition();
   manatee.position.x = point.x;
   manatee.position.y = point.y;
   requestAnimFrame(gameLoop);
